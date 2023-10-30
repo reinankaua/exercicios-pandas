9ed0cb9801451393857a32389e977779024f6ba8
rmorais <contato.reinankaua@gmail.com>
2023-10-30 06:32:13 -0300

    Adicionado novas questões

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import pandas as pd
 
 df = pd.read_excel('Notas.xlsx')
 
-print(f"Qual a quantidade de linhas e colunas desse dataframe? {df.shape}")
-print(f"Quais os tipos das váriaveis desse dataframe? {df.dtypes}")
+print(f"Qual a quantidade de linhas e colunas desse dataframe? \n{df.shape}\n\n")
+print(f"Quais os tipos das váriaveis desse dataframe? \n{df.dtypes}\n\n")
+print(f"Há valores nulos nesse dataframe? \n{df.isnull().sum()}\n\n")
+df1 = df[['genero', 'almoco']]
+print(f"Crie um novo dataframe chamado 'df1', esse dataframe deve possuir somente as colunas 'genero' e 'almoco'. \n{df1}\n\n")
+print(f"Ordene o dataset de forma crescente de acordo com a nota de redação. \n{df.sort_values(by='nota redacao')}\n\n")
+print(f"Qual é a maior nota em matemática? \n{df[['nota matematica']].max()}\n\n")
+print(f"Qual é a menor nota em literatura? \n{df[['nota literatura']].min()}\n\n")
+print(f"Qual é a soma de todas as notas em redacao? \n{df[['nota redacao']].sum()}\n\n")
+print(f"Qual é a nota em matemática do aluno de index 502?")
+
 
 """
-print(f"Há valores nulos nesse dataframe?")
-print(f"Crie um novo dataframe chamado 'df1', esse dataframe deve possuir somente as colunas 'genero' e 'almoco'.")
-print(f"Ordene o dataset de forma crescente de acordo com a nota de redação.")
-print(f"Qual é a maior nota em matemática?")
-print(f"Qual é a menor nota em literatura?")
-print(f"Qual é a soma de todas as notas em redacao?")
-print(f"Qual é a nota em matemática do aluno de index 502?")
 print(f"Quantos grupos étnicos existem no dataframe?")
 print(f"Dentro da coluna etnia, qual é o grupo que possui a maior quantidade de pessoas?Qual é o index da aluna que tirou a menor nota em literatura?")
 print(f"Qual é a escolaridade mais comum entre os pais dos alunos?")
